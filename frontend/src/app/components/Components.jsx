f4dae9e35295c7c95fdbf14cf87398e3339fe0b3
Prakhar54-byte <b23bb1032@iitj.ac.in>
2025-02-28 23:12:25 +0530

    initialize frontend project with Next.js and Tailwind CSS setup

---
+++ frontend/src/app/components/Components.jsx
@@ -0,0 +1,325 @@
+import React from "react";
+import { Check } from "../../components/Check";
+import { IconsNavigationOthers10Check } from "../../icons/IconsNavigationOthers10Check";
+import "./style.css";
+
+export const Component = () => {
+  return (
+    <div className="component">
+      <div className="overlap">
+        <div className="rectangle" />
+
+        <div className="illustration">
+          <div className="overlap-group">
+            <div className="div">
+              <div className="rectangle-2" />
+
+              <img
+                className="img"
+                alt="Rectangle"
+                src="https://c.animaapp.com/gs7GlUVi/img/rectangle-2@2x.png"
+              />
+
+              <img
+                className="vector"
+                alt="Vector"
+                src="https://c.animaapp.com/gs7GlUVi/img/vector-31.svg"
+              />
+
+              <img
+                className="vector-2"
+                alt="Vector"
+                src="https://c.animaapp.com/gs7GlUVi/img/vector-32.svg"
+              />
+
+              <img
+                className="vector-3"
+                alt="Vector"
+                src="https://c.animaapp.com/gs7GlUVi/img/vector-33.svg"
+              />
+
+              <img
+                className="vector-4"
+                alt="Vector"
+                src="https://c.animaapp.com/gs7GlUVi/img/vector-34.svg"
+              />
+
+              <img
+                className="group"
+                alt="Group"
+                src="https://c.animaapp.com/gs7GlUVi/img/group-3-1@2x.png"
+              />
+
+              <img
+                className="group-2"
+                alt="Group"
+                src="https://c.animaapp.com/gs7GlUVi/img/group-4-1@2x.png"
+              />
+
+              <div className="overlap-group-wrapper">
+                <div className="overlap-group-2">
+                  <img
+                    className="vector-5"
+                    alt="Vector"
+                    src="https://c.animaapp.com/gs7GlUVi/img/vector-35.svg"
+                  />
+
+                  <img
+                    className="vector-6"
+                    alt="Vector"
+                    src="https://c.animaapp.com/gs7GlUVi/img/vector-36.svg"
+                  />
+                </div>
+              </div>
+
+              <p className="text-wrapper">
+                Start for free and get attractive offers from the community
+              </p>
+
+              <img
+                className="vector-7"
+                alt="Vector"
+                src="https://c.animaapp.com/gs7GlUVi/img/vector-37.svg"
+              />
+
+              <img
+                className="vector-8"
+                alt="Vector"
+                src="https://c.animaapp.com/gs7GlUVi/img/vector-38.svg"
+              />
+
+              <img
+                className="vector-9"
+                alt="Vector"
+                src="https://c.animaapp.com/gs7GlUVi/img/vector-39.svg"
+              />
+
+              <img
+                className="vector-10"
+                alt="Vector"
+                src="https://c.animaapp.com/gs7GlUVi/img/vector-40.svg"
+              />
+
+              <img
+                className="vector-11"
+                alt="Vector"
+                src="https://c.animaapp.com/gs7GlUVi/img/vector-41.svg"
+              />
+
+              <img
+                className="vector-12"
+                alt="Vector"
+                src="https://c.animaapp.com/gs7GlUVi/img/vector-42.svg"
+              />
+
+              <img
+                className="group-3"
+                alt="Group"
+                src="https://c.animaapp.com/gs7GlUVi/img/group-5@2x.png"
+              />
+
+              <img
+                className="vector-13"
+                alt="Vector"
+                src="https://c.animaapp.com/gs7GlUVi/img/vector-43.svg"
+              />
+
+              <img
+                className="vector-14"
+                alt="Vector"
+                src="https://c.animaapp.com/gs7GlUVi/img/vector-44.svg"
+              />
+
+              <img
+                className="vector-15"
+                alt="Vector"
+                src="https://c.animaapp.com/gs7GlUVi/img/vector-45.svg"
+              />
+
+              <img
+                className="vector-16"
+                alt="Vector"
+                src="https://c.animaapp.com/gs7GlUVi/img/vector-46.svg"
+              />
+
+              <img
+                className="vector-17"
+                alt="Vector"
+                src="https://c.animaapp.com/gs7GlUVi/img/vector-47.svg"
+              />
+
+              <img
+                className="vector-18"
+                alt="Vector"
+                src="https://c.animaapp.com/gs7GlUVi/img/vector-48.svg"
+              />
+
+              <img
+                className="vector-19"
+                alt="Vector"
+                src="https://c.animaapp.com/gs7GlUVi/img/vector-49.svg"
+              />
+
+              <img
+                className="vector-20"
+                alt="Vector"
+                src="https://c.animaapp.com/gs7GlUVi/img/vector-50.svg"
+              />
+
+              <img
+                className="vector-21"
+                alt="Vector"
+                src="https://c.animaapp.com/gs7GlUVi/img/vector-51.svg"
+              />
+
+              <img
+                className="vector-22"
+                alt="Vector"
+                src="https://c.animaapp.com/gs7GlUVi/img/vector-52.svg"
+              />
+
+              <img
+                className="vector-23"
+                alt="Vector"
+                src="https://c.animaapp.com/gs7GlUVi/img/vector-53.svg"
+              />
+
+              <img
+                className="vector-24"
+                alt="Vector"
+                src="https://c.animaapp.com/gs7GlUVi/img/vector-54.svg"
+              />
+
+              <img
+                className="vector-25"
+                alt="Vector"
+                src="https://c.animaapp.com/gs7GlUVi/img/vector-55.svg"
+              />
+
+              <img
+                className="vector-26"
+                alt="Vector"
+                src="https://c.animaapp.com/gs7GlUVi/img/vector-56.svg"
+              />
+
+              <img
+                className="vector-27"
+                alt="Vector"
+                src="https://c.animaapp.com/gs7GlUVi/img/vector-57.svg"
+              />
+
+              <img
+                className="vector-28"
+                alt="Vector"
+                src="https://c.animaapp.com/gs7GlUVi/img/vector-58.svg"
+              />
+
+              <img
+                className="vector-29"
+                alt="Vector"
+                src="https://c.animaapp.com/gs7GlUVi/img/vector-60.svg"
+              />
+
+              <img
+                className="vector-30"
+                alt="Vector"
+                src="https://c.animaapp.com/gs7GlUVi/img/vector-61.svg"
+              />
+
+              <img
+                className="group-4"
+                alt="Group"
+                src="https://c.animaapp.com/gs7GlUVi/img/group-1-1@2x.png"
+              />
+
+              <img
+                className="group-5"
+                alt="Group"
+                src="https://c.animaapp.com/gs7GlUVi/img/group-2-1@2x.png"
+              />
+            </div>
+
+            <img
+              className="vector-31"
+              alt="Vector"
+              src="https://c.animaapp.com/gs7GlUVi/img/vector-59.svg"
+            />
+          </div>
+        </div>
+
+        <div className="frame">
+          <div className="frame-2">
+            <div className="frame-3">
+              <div className="text-wrapper-2">Login to your Account</div>
+
+              <p className="p">See what is going on with your business</p>
+            </div>
+
+            <div className="frame-4">
+              <img
+                className="image"
+                alt="Image"
+                src="https://c.animaapp.com/gs7GlUVi/img/image-2-1@2x.png"
+              />
+
+              <div className="text-wrapper-3">Continue with Google</div>
+            </div>
+          </div>
+
+          <p className="or-sign-in-with">
+            <span className="span">-------------</span>
+
+            <span className="text-wrapper-4"> or Sign in with Email </span>
+
+            <span className="span">------------- </span>
+          </p>
+
+          <div className="frame-5">
+            <div className="frame-6">
+              <div className="frame-7">
+                <div className="text-wrapper-5">Email</div>
+
+                <div className="div-wrapper">
+                  <div className="text-wrapper-6">mail@abc.com</div>
+                </div>
+              </div>
+
+              <div className="frame-8">
+                <div className="frame-7">
+                  <div className="text-wrapper-5">Password</div>
+
+                  <div className="frame-9">
+                    <div className="text-wrapper-7">*****************</div>
+                  </div>
+                </div>
+
+                <div className="frame-10">
+                  <div className="checkbox">
+                    <Check
+                      checkSolidClassName="design-component-instance-node"
+                      checked
+                      className="check-instance"
+                      icon={
+                        <IconsNavigationOthers10Check className="icons-navigation-others-10-check" />
+                      }
+                      size="twelve"
+                      state="enabled"
+                      type="selection-box"
+                    />
+                    <div className="text-wrapper-8">Remember Me</div>
+                  </div>
+
+                  <div className="text-wrapper-9">Forgot Password?</div>
+                </div>
+              </div>
+            </div>
+
+            <div className="frame-11">
+              <div className="text-wrapper-10">Login</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
